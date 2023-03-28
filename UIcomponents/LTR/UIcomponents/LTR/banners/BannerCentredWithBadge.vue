dccc639d1002aaf6998b7d183979021437dc148f
FlorianBEME <florianbme@gmail.com>
2023-03-28 21:24:38 +0200

    Fix

--- UIcomponents/LTR/UIcomponents/LTR/banners/BannerCentredWithBadge.vue
+++ UIcomponents/LTR/UIcomponents/LTR/banners/BannerCentredWithBadge.vue
@@ -25,9 +25,9 @@
                 class="w-5 h-5"
             >
               <path
-                  fillRule="evenodd"
+                  fill-rule="evenodd"
                   d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-                  clipRule="evenodd"
+                  clip-rule="evenodd"
               />
             </svg>
           </a>
--- UIcomponents/LTR/UIcomponents/LTR/banners/BannerWithUnderlineLink.vue
+++ UIcomponents/LTR/UIcomponents/LTR/banners/BannerWithUnderlineLink.vue
@@ -12,7 +12,7 @@
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="{1.5}"
+                        stroke-width="1.5"
                         stroke="currentColor"
                     >
                         <path
