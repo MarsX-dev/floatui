8d694b2fd6577bfcd9151f86a479551a87b428e8
ML <me.mlaure@gmail.com>
2023-03-28 19:47:11 +0100

    feat(vue): add 404 pages

---
+++ UIcomponents/RTL/404-pages/404PageWithLogo.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <div class="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
+      <div class="max-w-lg mx-auto text-center">
+        <div class="pb-6">
+          <img src="https://floatui.com/logo.svg" width="150" class="mx-auto">
+        </div>
+        <h3 class="text-gray-800 text-4xl font-semibold sm:text-5xl">
+          الصفحة غير موجودة
+        </h3>
+        <p class="text-gray-600 mt-3">
+          عذرا، الصفحة التي تبحث عنها لا يمكن العثور عليها أو قد تم إزالتها.
+        </p>
+      </div>
+    </div>
+  </main>
+</template>
