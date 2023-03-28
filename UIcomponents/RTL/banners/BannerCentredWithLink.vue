09d21b170830809046549c3ebf4aaa6def8f1ab4
FlorianBEME <florianbme@gmail.com>
2023-03-28 21:37:06 +0200

    adding RTL version

---
+++ UIcomponents/RTL/banners/BannerCentredWithLink.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-indigo-600">
+    <div class="max-w-screen-xl mx-auto px-4 py-3 items-center gap-x-4 justify-center text-white sm:flex md:px-8">
+      <p class="py-2 font-medium">
+        لقد أطلقنا للتو منتجنا الجديد! يمكنك التحقق من جميع الميزات الآن.
+      </p>
+      <a href="javascript:void(0)" class="flex-none inline-block w-full mt-3 py-2 px-3 text-center text-indigo-600 font-medium bg-white duration-150 hover:bg-gray-100 active:bg-gray-200 rounded-lg sm:w-auto sm:mt-0 sm:text-sm">
+        معرفة المزيد
+      </a>
+    </div>
+  </div>
+</template>
+
