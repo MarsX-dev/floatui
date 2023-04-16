9ec0cf34ba0e03388258ab953930679deee4d4c2
ML <me.mlaure@gmail.com>
2023-04-16 18:37:12 +0100

    vue: add alerts rtl

---
+++ UIcomponents/RTL/alerts/AlertDanger.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mt-12 mx-4 px-4 rounded-md border-l-4 border-red-500 bg-red-50 md:max-w-2xl md:mx-auto md:px-8">
+    <div class="flex justify-between py-3">
+      <div class="flex">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="self-center mr-3">
+          <span class="text-red-600 font-semibold">
+            خطأ
+          </span>
+          <p class="text-red-600 mt-1">
+            عذرا حدث خطأ ما، يرجى إدخال البريد الإلكتروني الصحيح.
+          </p>
+        </div>
+      </div>
+      <button class="self-start text-red-500">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
---
+++ UIcomponents/RTL/alerts/AlertDangerWithLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="max-w-5xl mx-auto px-4 md:px-8">
+    <div class="flex justify-between p-4 rounded-md bg-red-50 border border-red-300">
+      <div class="flex gap-3 sm:items-center">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </div>
+        <p class="text-red-600 sm:text-sm">
+          لا يوجد حساب مرتبط بعنوان البريد الإلكتروني هذا. <a href="/" class="underline font-medium hover:text-red-700" @click.prevent>تسجيل الدخول؟</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
