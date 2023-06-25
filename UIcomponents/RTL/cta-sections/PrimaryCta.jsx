f1d18750aae25c85d094665dad860c8e7b838b54
Triston Armstrong <triston95strong@gmail.com>
2023-06-24 22:27:51 -0500

    bulk update some svg props to be camelcase
    props updated: fill-rule, clip-rule, clip-path

--- UIcomponents/RTL/cta-sections/PrimaryCta.jsx
+++ UIcomponents/RTL/cta-sections/PrimaryCta.jsx
@@ -15,7 +15,7 @@ export default () => {
                     href="javascript:void()">
                     جرب اﻵن
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mr-1 duration-150" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd" />
                     </svg>
                 </a>
             </div>
--- templates/split/components/ui/Pricing/index.jsx
+++ templates/split/components/ui/Pricing/index.jsx
@@ -88,9 +88,9 @@ const Pricing = () => {
                                                         viewBox='0 0 20 20'
                                                         fill='currentColor'>
                                                         <path
-                                                            fill-rule='evenodd'
+                                                            fillRule='evenodd'
                                                             d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
-                                                            clip-rule='evenodd'></path>
+                                                            clipRule='evenodd'></path>
                                                     </svg>
                                                     {featureItem}
                                                 </li>
