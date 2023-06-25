ad48f51f7b429488eca11cca3e384aa207aa0d6e
Triston Armstrong <triston95strong@gmail.com>
2023-06-24 22:21:58 -0500

    bulk update the fill-rule svg prop to fillRule

--- UIcomponents/RTL/cards/BlogCardsSecondary.jsx
+++ UIcomponents/RTL/cards/BlogCardsSecondary.jsx
@@ -57,7 +57,7 @@ export default () => {
                                 <button className="mt-2 outline-none flex items-center text-[14px] text-blue-600 decoration-blue-600 hover:underline">
                                     اقرأ المزيد
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+                                        <path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
                                     </svg>
                                 </button>
                             </a>
